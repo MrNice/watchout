68d53e9905e247360c57fca10867b0b7fc97f2a1
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-07 15:34:27 -0800

    Added control flow functions

--- watchout.js
+++ watchout.js
@@ -1,16 +1,32 @@
+var svg;
 var width  = 600;
 var height = 500;
 
-var svg = d3.select('body').append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" +  (width / 2) + "," + (height / 2) + ")");
-
+// DATA CREATION 
 var dotFactory = function(nDots){
   return _(_.range(nDots)).map(function(aDot){
-    return {name:aDot};
+    return aDot;
   });
 };
+var initialize = function() {
+  return d3.select('body')
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append('g');
+}
 
-// extensions of dotFactory
+var update = function(dotData) {
+  // Join data
+  var dots = svg.select('g').selectAll('circles')
+    .data(dotData, function(d) {return d;});
+
+  dots.enter()
+    .append('circle')
+    .attr('cx', function(){return Math.random()*width;})
+    .attr('cy', function(){return Math.random()*height;})
+    .attr('r', 20) // width = f(innerWidth) := width / 200
+    .attr('fill', 'black');
+};
+svg = initialize();
+update(dotFactory(15));
