b30c2d3c75ea913f88da6c7c51600c3ec55a012c
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-07 14:59:36 -0800

    scaffolded libs + svg and dotFactory

--- watchout.js
+++ watchout.js
@@ -1,2 +1,16 @@
-// start slingin' some d3 here.
+var width  = 600;
+var height = 500;
 
+var svg = d3.select('body').append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" +  (width / 2) + "," + (height / 2) + ")");
+
+var dotFactory = function(nDots){
+  return _(_.range(nDots)).map(function(aDot){
+    return {name:aDot};
+  });
+};
+
+// extensions of dotFactory
