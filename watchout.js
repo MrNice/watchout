c248d377decb9ef83d97d28c3faeba36b818c23f
Fabrice Colas <pub@grano-salis.net>
2014-03-07 16:16:13 -0800

    refactor, dots translate on the board

--- watchout.js
+++ watchout.js
@@ -1,32 +1,62 @@
-var svg;
-var width  = 600;
-var height = 500;
-
-// DATA CREATION 
+/******************************************************************************/
+/* GAME                                                                       */
+/******************************************************************************/
 var dotFactory = function(nDots){
   return _(_.range(nDots)).map(function(aDot){
     return aDot;
   });
 };
-var initialize = function() {
-  return d3.select('body')
+
+var initSvg = function() {
+  var svg = d3.select('body')
     .append("svg")
     .attr("width", width)
     .attr("height", height)
     .append('g');
-}
+  return svg;
+};
 
-var update = function(dotData) {
-  // Join data
-  var dots = svg.select('g').selectAll('circles')
-    .data(dotData, function(d) {return d;});
-
-  dots.enter()
+var initDots = function(data){
+  // DATA JOIN: Join data with new elements if any
+  var dots = svg
+    .selectAll('g')
+    .data(data, function(d) {return d;});
+  //
+  // ENTER: Create new elements as needed
+  dots
+    .enter()
     .append('circle')
     .attr('cx', function(){return Math.random()*width;})
     .attr('cy', function(){return Math.random()*height;})
-    .attr('r', 20) // width = f(innerWidth) := width / 200
+    .attr('r',  width / 100)
     .attr('fill', 'black');
+  return dots;
 };
-svg = initialize();
-update(dotFactory(15));
+
+var update = function(dots) {
+  var x = Math.random()*width;
+  var y = Math.random()*height;
+  dots
+    .transition()
+    .attr('transform','translate(' + x + ', ' + y + ')');
+  return dots;
+};
+
+/******************************************************************************/
+/* GLOBAL                                                                     */
+/******************************************************************************/
+var width  = 600;
+var height = 500;
+var timeInterval = 1000;
+var dataset = dotFactory(15);
+
+/******************************************************************************/
+/* RUNTIME                                                                    */
+/******************************************************************************/
+// INIT & FIRST UPDATE 
+var svg = initSvg();
+var dots = initDots(dataset);
+
+
+// TIMER: Randomize dots' position
+setInterval(function() { return update(dots); }, timeInterval);
